14c748a66e7ed68fa537f43209c48bd29c78e4fc
张胜 <zhangsheng@192.168.0.106>
2019-02-17 17:22:21 +0800

    add array and list for line structure

---
+++ 02_array_and_list_line_structure/array_and_list.py
@@ -0,0 +1,32 @@
+# coding:utf-8
+# 线性结构：array and list，数组和列表
+
+
+class Array(object):
+    def __init__(self, size=32):
+        self._size = size
+        self._items = [None] * size
+
+    def __getitem__(self, index):
+        return self._items[index]
+
+    def __setitem__(self, index, value):
+        self._items[index] = value
+
+    def clear(self, value=None):
+        for i in range(len(self._items)):
+            self._items[i] = value
+
+    def __iter__(self):
+        for item in self._items:
+            yield item
+
+
+def test_array():
+    size = 10
+    a = Array(size)
+    a[0] = 1
+    assert a[0] == 1
+
+    a.clear()
+    assert a[0] is None
