f560a65e4341c2856b518883cd072889f3d4ffe4
张胜 <zhangsheng@192.168.0.106>
2019-02-23 19:37:55 +0800

    add LRU and topk but unfinished

---
+++ 15_heap/lfu.py
@@ -0,0 +1,6 @@
+# coding:utf-8
+# @Time : 2019/2/23 19:36 
+# @Author : Andy.Zhang
+# @Desc : LRU(least frequently used)，当缓存满了之后剔除一个最少使用的 key #todo
+
+
---
+++ 15_heap/topk.py
@@ -0,0 +1,6 @@
+# coding:utf-8
+# @Time : 2019/2/23 19:35 
+# @Author : Andy.Zhang
+# @Desc : #todo
+
+
